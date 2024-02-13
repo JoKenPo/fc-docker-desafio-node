1057fab2992fbc58da054671db9754bbe103e943
Eduardo Almeida <duduf2005@hotmail.com>
2024-02-13 03:55:44 -0300

    feat: desafio node

---
+++ node/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
+
+const nomes = ["Eduardo", "Wesley", "Marcia", "Daniel", "Camila", "Dany"];
+let peoples = [];
+
+app.get("/", async (req, res) => {
+  const sql = `INSERT INTO people(name) values("${
+    nomes[Math.round(Math.random(6) * (5 - 0) + 0)]
+  }")`;
+  const peopleSQL = `SELECT name FROM people`;
+
+  await connection.query(sql, peopleSQL, async () => {
+    await connection.query(peopleSQL, (err, rows) => {
+      peoples = rows;
+      connection.end();
+
+      res.send(
+        `<h1>Full Cycle Rocks!</h1><ul>
+        ${peoples.map((people) => {
+          return `<li>${people.name}</li>`;
+        })}
+        </ul>`
+      );
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
